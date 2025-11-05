78f9c8aa228ad90d4e3c494c0d430a9dbaaf1c17
micxky9 <michaelalih5@gmail.com>
2025-11-05 12:00:31 +0100

    Delete SPAG KING PROJECT/homescript.js

--- SPAG KING PROJECT/homescript.js
+++
@@ -1,9 +0,0 @@
-function showSideBar(){
-	const bar = document.querySelector('.sidebar');
-	bar.style.display = 'flex'; 
-
-}
-function hide(){
-	const bar = document.querySelector('.sidebar');
-	bar.style.display = 'none'; 
-}
