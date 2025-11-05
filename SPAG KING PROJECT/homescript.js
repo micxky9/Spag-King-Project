69ae6d8e58085d69bf551341b55d67804aa12fe8
micxky9 <michaelalih5@gmail.com>
2025-11-05 11:59:32 +0100

    Add files via upload

---
+++ SPAG KING PROJECT/homescript.js
@@ -0,0 +1,9 @@
+function showSideBar(){
+	const bar = document.querySelector('.sidebar');
+	bar.style.display = 'flex'; 
+
+}
+function hide(){
+	const bar = document.querySelector('.sidebar');
+	bar.style.display = 'none'; 
+}
